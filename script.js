28a99738c5de8482489b3527c504bcd36b0ceaf5
Manahil Aleem <manahilaleem1@gmail.com>
2024-12-15 17:47:37 +0500

    deploy

---
+++ script.js
@@ -0,0 +1,19 @@
+//1
+let var1,var2,var3; 
+
+//2
+//legal vaiable names
+let myname123,_myname,$myname,my_name,myname;
+//Illegal variable name
+// let 123age; //starts with number
+// let my age; //contains space
+// let var; //reserved keyword
+// let @age; //special character
+// let my-variable; //hyphen
+
+//3
+document.write("<h1>Rules for naming JS variables</h1><br><br>");
+document.write("Variable names can only contain letters,numbers,$ and _ For Example $my-1stVariable<br>");
+document.write("Variables must begin with a letter,& or _. For example $name, _name or name <br>");
+document.write("Variable names are case sensitive <br>"); 
+document.write("Variable names should not be JS keywords");
